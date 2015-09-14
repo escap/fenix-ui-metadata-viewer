931177f6557fe2cf3f33a565418eeefabab7a40c
Kalimaha <guido.barbaglia@gmail.com>
2015-09-14 16:38:35 +0200

    FENIX standards applied.

--- js/paths.js
+++ js/paths.js
@@ -6,7 +6,7 @@ define(function () {
     var config;
     config = {
         paths: {
-            FENIX_UI_METADATA_VIEWER: 'fenix-ui-metadata-viewer',
+            FENIX_UI_METADATA_VIEWER: 'start',
             fenix_ui_metadata_viewer: '../',
             sweetAlert: 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/0.5.0/sweet-alert.min',
             jsonEditor: '{FENIX_CDN}/js/json-editor/0.7.17/jsoneditor',
