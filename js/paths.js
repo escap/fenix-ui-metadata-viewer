6a6cbca4f936c29edff6817dd57c878837349ebf
danielefenix <daniele.salvatore@fao.org>
2015-09-24 18:31:12 +0200

    .

--- js/paths.js
+++ js/paths.js
@@ -6,11 +6,10 @@ define(function () {
     var config;
     config = {
         paths: {
-            FENIX_UI_METADATA_VIEWER: 'start',
+            FENIX_UI_METADATA_VIEWER:'./start',
             fenix_ui_metadata_viewer: '../',
             sweetAlert: 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/0.5.0/sweet-alert.min',
-            jsonEditor: '{FENIX_CDN}/js/json-editor/0.7.17/jsoneditor',
-            FAOSTAT_THEME: '../submodules/json-editor-faostat-theme/js/faostat-theme'
+            jsonEditor: '{FENIX_CDN}/js/json-editor/0.7.17/jsoneditor'
         },
         shim: {
             bootstrap: {
@@ -25,3 +24,16 @@ define(function () {
     return config;
 
 });
+
+
+
+/*
+*
+*  'fx-menu/start': './start',
+ 'fx-menu/templates': '../templates/',
+
+ 'jquery': '{FENIX_CDN}/js/jquery/2.1.1/jquery.min',
+ 'bootstrap': '{FENIX_CDN}/js/bootstrap/3.2/js/bootstrap.min',
+ 'text': '{FENIX_CDN}/js/requirejs/plugins/text/2.0.12/text',
+ 'amplify' : '{FENIX_CDN}/js/amplify/1.1.2/amplify.min'
+ },*/
